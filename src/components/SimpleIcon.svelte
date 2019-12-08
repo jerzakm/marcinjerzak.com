5ddc4dc42ef6dab637e13123e4510e87935182c7
Marcin <jerzakm@gmail.com>
2019-12-08 19:14:02 +0100

    social icons

---
+++ src/components/SimpleIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+  import * as simpleIcons from "simple-icons";
+  export let icon;
+  const iconPath = simpleIcons.default[`${icon}`].path;
+</script>
+
+<style>
+  svg {
+    fill: white;
+    width: 3em;
+  }
+  .simple-icon {
+    margin: 0 0.5rem;
+  }
+</style>
+
+<span class="simple-icon">
+  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d={iconPath} />
+  </svg>
+</span>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,66 @@
+<script>
+  import SimpleIcon from "../components/SimpleIcon.svelte";
+</script>
+
 <style lang="scss">
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  h2,
+  h3 {
+    margin: 0;
+    text-align: center;
+  }
+  h1 {
+    text-transform: uppercase;
+  }
+  h2 {
+    font-size: 2.5em;
+  }
+  h3 {
+    font-size: 1em;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  .home-container {
+    min-height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  .initials-logo {
+    position: absolute;
+    padding: 1.5rem 2.2rem;
+    font-size: 2em;
+    font-weight: 700;
+  }
+
+  .social-container {
+    display: flex;
+    margin: 2rem;
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Portfolio</title>
 </svelte:head>
 
-<h1>Profound mission statement.</h1>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque assumenda est ab ut recusandae laborum autem! Velit eum voluptates ducimus? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, laborum.</p>
-<!-- <figure>
-	<img alt='Borat' src='img.jpg'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure> -->
+<span class="initials-logo">MJ</span>
+
+<section class="home-container">
+  <div>
+    <h2 class="primary-c">software developer</h2>
+    <h3>with a</h3>
+    <h1>Profound mission statement.</h1>
+    <h3 class="primary-c">
+      tbh, sometimes I'll start to code and I don't even know where it's going.
+      I just hope I find it along the way.
+    </h3>
+  </div>
+  <div class="social-container">
+    <SimpleIcon icon={'GitHub'} />
+    <SimpleIcon icon={'Twitter'} />
+    <SimpleIcon icon={'dev.to'} />
+    <SimpleIcon icon={'Itch.io'} />
+    <SimpleIcon icon={'LinkedIn'} />
+  </div>
+</section>
