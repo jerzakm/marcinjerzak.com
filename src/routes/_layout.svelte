6e6b1d61f9c5b830ca932ac196f8abf1100531f0
Marcin <jerzakm@gmail.com>
2020-06-27 15:25:31 +0200

    layout cleanup and waterBg reactivated

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -8,44 +8,21 @@
   }
 </style>
 
-<script context="module">
-  export async function preload({ params, query }) {
-    const imgRes = await this.fetch("imgList.json");
-    const imgData = await imgRes.json();
-
-    if (imgRes.status === 200) {
-      return {
-        imgList: imgData
-      };
-    } else {
-      this.error(imgData.status, imgData.message);
-    }
-  }
-</script>
-
 <script>
-  // import WaterBg from "./../components/WaterBg.svelte";
+  import WaterBg from "./../components/WaterBg.svelte";
   import Nav from "../components/Nav.svelte";
   import { onMount } from "svelte";
   export let imgList;
-  // window.imgList = imgList;
 
   let mounted;
-
-  onMount(() => {
-    mounted = true;
-    window.imgList = imgList;
-  });
 
   export let segment;
   `${segment}`;
 </script>
 
 <Nav segment="segment" />
-<!-- <WaterBg /> -->
+<WaterBg />
 
-{#if mounted}
-  <main>
-    <slot />
-  </main>
-{/if}
+<main>
+  <slot />
+</main>
